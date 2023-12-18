612879162d09a0b4512c493acb17dc1f56eae587
fannybaquey <30581629-fannybaquey@users.noreply.replit.com>
2023-12-18 17:35:45 +0000

    First commit: Created a Python program to generate a band name from two pieces of information.
    DAY 1 of 100 days of coding in Python.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #1. Create a greeting for your program.
-
+print("Welcome to Band name generator program. \n\nWith just two questions, I'll create the most incredible band name for you! \n\nLet's get started!")
 #2. Ask the user for the city that they grew up in.
-
+city_name = input("Where did you grow up ?\n")
 #3. Ask the user for the name of a pet.
-
+pet_name = input("What is your pet's name ?\n")
 #4. Combine the name of their city and pet and show them their band name.
-
+band_name = city_name + " " + pet_name
 #5. Make sure the input cursor shows on a new line:
-
+print("The name of you band is: " + band_name)
 # Solution: https://replit.com/@appbrewery/band-name-generator-end
+
+
+
+
+
+
 
